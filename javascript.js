e4538d92b3e71103bd4ca0c1a9f6cb19d952735e
nanaBazzov <102252308+nanaBazzov@users.noreply.github.com>
2022-06-21 16:43:50 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+document.addEventListener("click",cat);
+function cat() {
+    fetch("https://catfact.ninja/fact")
+        .then((res) => res.json())
+        .then((data) => {
+            document.getElementById("fact").innerHTML = data.fact;
+        });
+}
